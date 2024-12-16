f7ba184cc5654df99da142f73ae03540ba846049
sipamungkas <sipamungkas.dev@gmail.com>
2024-12-16 08:11:21 +0700

    feat(auth): supabase sign in integration

---
+++ src/app/auth/sign-in/action.ts
@@ -0,0 +1,73 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { z } from "zod";
+
+import { createClient } from "@/lib/utils/supabase/server";
+
+const schema = z.object({
+  email: z
+    .string({
+      invalid_type_error: "Invalid Email",
+    })
+    .email(),
+  password: z
+    .string({
+      invalid_type_error: "Invalid Password",
+    })
+    .min(8, {
+      message: "Invalid Password",
+    }),
+});
+
+export async function login(prevState: any, formData: FormData) {
+  const validatedFields = schema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  // Return early if the form data is invalid
+  if (!validatedFields.success) {
+    return { message: "Invalid Email or Password" };
+  }
+
+  //   const supabase = await createClient();
+
+  //   // type-casting here for convenience
+  //   // in practice, you should validate your inputs
+  //   const data = {
+  //     email: validatedFields.data.email,
+  //     password: validatedFields.data.password,
+  //   };
+
+  //   const { error } = await supabase.auth.signInWithPassword(data);
+  //   console.log({ error });
+
+  //   if (error) {
+  //     return { message: "Invalid Email or Password" };
+  //   }
+
+  revalidatePath("/", "layout");
+  redirect("/account");
+}
+
+export async function signup(formData: FormData) {
+  const supabase = await createClient();
+
+  // type-casting here for convenience
+  // in practice, you should validate your inputs
+  const data = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signUp(data);
+
+  if (error) {
+    redirect("/error");
+  }
+
+  revalidatePath("/", "layout");
+  redirect("/account");
+}
---
+++ src/app/auth/sign-in/sign-in-form.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { login } from "./action";
+import { useActionState } from "react";
+import { LoaderCircle, TriangleAlert } from "lucide-react";
+
+const initialState = {
+  message: "",
+};
+
+export const SignInForm = () => {
+  const [state, formAction, pending] = useActionState(login, initialState);
+
+  return (
+    <Card className="w-96">
+      <CardHeader>
+        <CardTitle>Sign In</CardTitle>
+        {state.message && !pending && (
+          <CardDescription>
+            <div className="bg-red-100 p-2 rounded-md grid grid-flow-col auto-cols-max gap-2 items-center mt-2">
+              <TriangleAlert className="h-4 w-4 text-red-500" />
+              <p className="text-red-500">{state.message}</p>
+            </div>
+          </CardDescription>
+        )}
+      </CardHeader>
+      <form>
+        <CardContent>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                placeholder="mail@sipamungkas.com"
+                required
+              />
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="framework">Password</Label>
+              <Input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="********"
+                required
+              />
+            </div>
+          </div>
+        </CardContent>
+        <CardFooter className="grid gap-4">
+          <Button disabled={pending} formAction={formAction}>
+            {pending ? <LoaderCircle className="animate-spin" /> : "Sign In"}
+          </Button>
+        </CardFooter>
+      </form>
+    </Card>
+  );
+};
