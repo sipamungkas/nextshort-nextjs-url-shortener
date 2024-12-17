f71f0518a8d45b391081dbd82e1073a50edbcd87
sipamungkas <sipamungkas.dev@gmail.com>
2024-12-17 13:02:47 +0700

    feat(home): add bio link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,68 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="w-full min-h-screen p-12 relative flex flex-col items-center">
+      <div className="max-w-xl">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src="/android-chrome-512x512.png"
+          width={120}
+          height={120}
+          alt="YukCek!"
+          className="mx-auto rounded-full"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="font-sans font-semibold text-3xl text-center text-foreground mt-8">
+          Yuk Cek!
+        </h1>
+        <p className="text-center mt-4">
+          Server terbaik dan termurah untuk development dan production-mu!
+        </p>
+        <div className="w-full mt-8 grid grid-col gap-6">
+          <Link href="https://yukcek.com/orangevps" target="_blank">
+            <Button className="w-full p-7 text-base font-semibold rounded-sm transition-all ease-in-out duration-500 hover:scale-105">
+              VPS Luar Negeri 1$-an
+            </Button>
+          </Link>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <Link href="https://yukcek.com/flaz" target="_blank">
+            <Button className="w-full p-7 text-base font-semibold rounded-sm transition-all ease-in-out duration-500 hover:scale-105">
+              VPS Indonesia Termurah
+            </Button>
+          </Link>
+
+          <Link href="https://yukcek.com/contabo" target="_blank">
+            <Button className="w-full p-7 text-base font-semibold rounded-sm transition-all ease-in-out duration-500 hover:scale-105">
+              VPS Termurah
+            </Button>
+          </Link>
+
+          <Link href="https://yukcek.com/onidel" target="_blank">
+            <Button className="w-full p-7 text-base font-semibold rounded-sm transition-all ease-in-out duration-500 hover:scale-105">
+              VPS SLA Uptime 100%
+            </Button>
+          </Link>
+
+          <Link href="https://yukcek.com/nevacloud" target="_blank">
+            <Button className="w-full p-7 text-base font-semibold rounded-sm transition-all ease-in-out duration-500 hover:scale-105">
+              VPS Indonesia terbaik
+            </Button>
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <footer className="flex flex-col items-center w-full mt-20">
+          <span className="text-white/80 text-center text-sm font-semibold">
+            © 2024 {""}
+            <Link
+              className="text-blue-300 "
+              href="https://sipamungkas.com"
+              target="_blank"
+            >
+              Sipamungkas
+            </Link>
+          </span>
+        </footer>
+      </div>
+    </main>
   );
 }
